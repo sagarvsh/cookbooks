04c77b6dd3a2cf51c6430cf4c389245a86782b22
sagarvsh <vs.sagar@gmail.com>
2019-10-08 04:07:00 +0000

    Get hostname and ip address

--- workstation/recipes/setup.rb
+++ workstation/recipes/setup.rb
@@ -11,8 +11,20 @@ package 'git' do
   action:install
 end
 
+#node['ipaddress']
+#node['memory']['total']
+
+# print statement 'I have 3 laptops'
+#laptop_count = 4
+#puts "I have #{laptop_count} laptops"
+
 file '/etc/motd' do
-	content 'This server is the property of ...'
+	content "This server is the property of Sagar
+	HOSTNAME: #{node['hostname']}
+	IPADDRESS: #{node['ipaddress']}
+	CPU: #{node['cpu']['0']['mhz']}
+	MEMORY: #{node['memory']['total']} 
+"
 	action :create
 	owner 'root'
 	group 'root'
