4fc524c9d758ee48003d23431515f8b545e4c857
sagarvsh <vs.sagar@gmail.com>
2019-10-08 02:35:04 +0000

    Initial commit

---
+++ workstation/recipes/setup.rb
@@ -0,0 +1,19 @@
+package 'vim-enhanced'
+package 'emacs'
+package 'nano'
+package 'tree' do
+	action :install
+end
+
+package 'ntp'
+
+package 'git' do
+  action:install
+end
+
+file '/etc/motd' do
+	content 'This server is the property of ...'
+	action :create
+	owner 'root'
+	group 'root'
+end
